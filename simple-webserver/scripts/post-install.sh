83f987cf280ec3d5cd11639212130d570e203aeb
James Kingsmill <james.kingsmill@gmail.com>
2016-12-15 16:08:04 +1100

    Add simple-webserver example

---
+++ simple-webserver/scripts/post-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo mv /tmp/index.html /var/www/html/index.html
+
+sudo service apache2 restart
---
+++ simple-webserver/scripts/pre-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Install apache
+sudo apt-get install -y apache2
+sudo apt-get install -y apache2-utils
