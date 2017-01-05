3ea450eb91bba7ae3e278dd26b62820c997f9ec9
James Kingsmill <james.kingsmill@gmail.com>
2017-01-05 14:15:57 +1100

    Fix typo in SSH SG name

--- two-tier-vpc/main.tf
+++ two-tier-vpc/main.tf
@@ -19,6 +19,6 @@ module "asg" {
   public_subnet_ids = "${module.vpc.public_subnet_ids}"
   http_inbound_sg_id = "${module.vpc.elb_http_inbound_sg_id}"
   https_inbound_sg_id = "${module.vpc.elb_https_inbound_sg_id}"
-  app_ssh_inbound_sg = "${module.vpc.app_ssh_inbound_sg}"
+  app_ssh_inbound_sg_id = "${module.vpc.app_ssh_inbound_sg_id}"
   outbound_sg_id = "${module.vpc.elb_outbound_sg_id}"
 }
