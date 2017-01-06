87ee949c25bb30a1233b6066efb50685118eec89
James Kingsmill <james.kingsmill@gmail.com>
2017-01-06 11:22:06 +1100

    Add tags to all resources, fix indentation

--- two-tier-vpc/main.tf
+++ two-tier-vpc/main.tf
@@ -1,25 +1,25 @@
 provider "aws" {
-  region = "${var.region}"
+  region                 = "${var.region}"
 }
 
 module "vpc" {
-  source = "../modules/vpc"
-  stack_name = "${var.stack_name}"
-  owner = "${var.owner}"
-  key_name = "${var.key_name}"
-  nat_gw_count = "1"
-  enable_jumpbox = true
+  source                 = "../modules/vpc"
+  stack_name             = "${var.stack_name}"
+  owner                  = "${var.owner}"
+  key_name               = "${var.key_name}"
+  nat_gw_count           = "1"
+  enable_jumpbox         = true
 }
 
 module "asg" {
-  source = "../modules/asg"
-  stack_name = "${var.stack_name}"
-  owner = "${var.owner}"
-  key_name = "${var.key_name}"
-  public_subnet_ids = "${module.vpc.public_subnet_ids}"
-  private_subnet_ids = "${module.vpc.private_subnet_ids}"
-  http_inbound_sg_id = "${module.vpc.elb_http_inbound_sg_id}"
-  https_inbound_sg_id = "${module.vpc.elb_https_inbound_sg_id}"
-  app_ssh_inbound_sg_id = "${module.vpc.app_ssh_inbound_sg_id}"
-  outbound_sg_id = "${module.vpc.elb_outbound_sg_id}"
+  source                 = "../modules/asg"
+  stack_name             = "${var.stack_name}"
+  owner                  = "${var.owner}"
+  key_name               = "${var.key_name}"
+  public_subnet_ids      = "${module.vpc.public_subnet_ids}"
+  private_subnet_ids     = "${module.vpc.private_subnet_ids}"
+  http_inbound_sg_id     = "${module.vpc.elb_http_inbound_sg_id}"
+  https_inbound_sg_id    = "${module.vpc.elb_https_inbound_sg_id}"
+  app_ssh_inbound_sg_id  = "${module.vpc.app_ssh_inbound_sg_id}"
+  outbound_sg_id         = "${module.vpc.elb_outbound_sg_id}"
 }
--- two-tier-vpc/outputs.tf
+++ two-tier-vpc/outputs.tf
@@ -1,7 +1,7 @@
 output "elb_dns_name" {
-  value = "${module.asg.elb_dns_name}"
+  value                  = "${module.asg.elb_dns_name}"
 }
 
 output "jumpbox_ip" {
-  value = "${module.vpc.jumpbox_ip}"
+  value                  = "${module.vpc.jumpbox_ip}"
 }
