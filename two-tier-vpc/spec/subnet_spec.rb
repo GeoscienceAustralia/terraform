1a3371a60afadf22d7d34f25fd787c1e06181ec9
Thomas Butler <tombutler.development@gmail.com>
2017-01-16 13:30:43 +1100

    Update Terraform version, fix bug in subnet spec

--- two-tier-vpc/spec/subnet_spec.rb
+++ two-tier-vpc/spec/subnet_spec.rb
@@ -1,7 +1,7 @@
 describe subnet('twotiertest-public-subnet-dev-ap-southeast-2c') do
   it { should exist }
   it { should be_available }
-  it { should belong_to_vpc('twotiertest-vpc') }}
+  it { should belong_to_vpc('twotiertest-vpc') }
 end
 
 describe subnet('twotiertest-private-subnet-dev-ap-southeast-2b') do
