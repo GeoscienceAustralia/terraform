80fa493ec63e2018669dbe08f28270bdcc371599
Thomas Butler <tombutler.development@gmail.com>
2017-01-27 11:56:29 +1100

    Add policy, attach to role, move profile to it's own file

--- modules/asg/launch_configuration.tf
+++ modules/asg/launch_configuration.tf
@@ -16,33 +16,7 @@ resource "aws_launch_configuration" "lc" {
   user_data                   = "${file(var.userdata_filepath)}"
   key_name                    = "${var.key_name}"
   associate_public_ip_address = true
-  iam_instance_profile        = "${aws_iam_instance_profile.read_bucket.id}"
-}
 
-resource "aws_iam_instance_profile" "read_bucket" {
-  name  = "${var.stack_name}_instance_profile"
-  roles = ["${var.bucket_name != "0" ? aws_iam_role.s3_readonly_role.name : ""}"]
-}
-
-resource "aws_iam_role" "s3_readonly_role" {
-  count = "${replace(var.bucket_name, "/^[^0].*/", "1")}"
-  name  = "${var.stack_name}_s3_readonly_role"
-  path  = "/"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2017-01-27",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:Get*",
-        "s3:List*"
-      ],
-      "Resource": "arn:aws:s3:::${var.bucket_name}"
-    }
-  ]
-
-}
-EOF
+  # This is defined in ./iam_instance_profile.tf
+  iam_instance_profile = "${aws_iam_instance_profile.read_bucket.id}"
 }
