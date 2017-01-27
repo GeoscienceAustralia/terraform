4bf8d96862029d57bfa86a5cc752ea44dd94210f
Thomas Butler <tombutler.development@gmail.com>
2017-01-27 11:35:31 +1100

    Fix role count check

--- modules/asg/launch_configuration.tf
+++ modules/asg/launch_configuration.tf
@@ -25,7 +25,7 @@ resource "aws_iam_instance_profile" "read_bucket" {
 }
 
 resource "aws_iam_role" "s3_readonly_role" {
-  count = "${replace("/^[^0].*/", "1")}"
+  count = "${replace(var.bucket_name, "/^[^0].*/", "1")}"
   name  = "${var.stack_name}_s3_readonly_role"
   path  = "/"
 
