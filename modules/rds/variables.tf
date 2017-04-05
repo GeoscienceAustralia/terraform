c9a2bc978d6570d68cb8542e810094cecd506f49
James Kingsmill <james.kingsmill@gmail.com>
2017-04-05 11:51:17 +1000

    Set RDS version, skip final snapshot flag

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -5,67 +5,67 @@
 // RDS instance variables
 
 variable "identifier" {
-  default          = "mydb-rds"
-  description      = "Identifier for your DB"
+  default     = "mydb-rds"
+  description = "Identifier for your DB"
 }
 
 variable "storage" {
-  default          = "10"
-  description      = "Storage size in GB"
+  default     = "10"
+  description = "Storage size in GB"
 }
 
 variable "engine" {
-  default          = "postgres"
-  description      = "Engine type, example values mysql, postgres"
+  default     = "postgres"
+  description = "Engine type, example values mysql, postgres"
 }
 
 variable "engine_version" {
-  description      = "Engine version"
+  description = "Engine version"
 
   default = {
-    mysql          = "5.6.22"
-    postgres       = "9.4.1"
+    mysql    = "5.6.22"
+    postgres = "9.5.4"
   }
 }
 
 variable "instance_class" {
-  default          = "db.t2.micro"
-  description      = "Instance class"
+  default     = "db.t2.micro"
+  description = "Instance class"
 }
 
 variable "db_name" {
-  default          = "mydb"
-  description      = "db name"
+  default     = "mydb"
+  description = "db name"
 }
 
 variable "username" {
-  default          = "myuser"
-  description      = "User name"
+  default     = "myuser"
+  description = "User name"
 }
 
 variable "password" {
-  description      = "password, provide through your ENV variables"
-  default          = "Password123"
+  description = "password, provide through your ENV variables"
+  default     = "Password123"
 }
 
 variable "rds_is_multi_az" {
-  default          = "false"
+  default = "false"
 }
 
 variable "cidr_blocks" {
-  default          = "0.0.0.0/0"
-  description      = "CIDR for sg"
+  default     = "0.0.0.0/0"
+  description = "CIDR for sg"
 }
 
 variable "sg_name" {
-  default          = "rds_sg"
-  description      = "Tag Name for sg"
+  default     = "rds_sg"
+  description = "Tag Name for sg"
 }
 
 // Variables from other modules
 
 variable "private_subnet_ids" {
-  type             = "list"
+  type = "list"
 }
 
 // Variables for providers used in this module
